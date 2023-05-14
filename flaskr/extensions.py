7ada070e94924329c056d5bdeed5830feac605b2
Mr.Wuliu_DESKTOP <mr_wuliu@foxmail.com>
2023-05-14 20:25:36 +0800

    Changes by Mr.Wuliu

--- flaskr/extensions.py
+++ flaskr/extensions.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap5
+
 db = SQLAlchemy()
 socketio = SocketIO()
 login_manager = LoginManager()
+bootstrap = Bootstrap5()
 
 @login_manager.user_loader
 def load_user(id):
--- flaskr/models.py
+++ flaskr/models.py
@@ -2,22 +2,8 @@ from flaskr.extensions import db
 from flask_login import UserMixin, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-# from flask_login import UserMixin
-# class EntityBase(object):
-#     def to_json(self):
-#         fields = self.__dict__
-#         if "_sa_instance_state" in fields:
-#             del fields["_sa_instance_state"]
-#         return fields
 
-# class User(db.Model):
-#     __tablename__ = 'user'
-#     id = db.Column(db.INTEGER,primary_key=True)
-#     username = db.Column(db.String(80),unique=True)
-#     password = db.Column(db.String(80),nullable=False)
-
-
-class User(db.Model,UserMixin): #, EntityBase):
+class User(db.Model,UserMixin):
     #数据表明、字段
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
@@ -25,9 +11,6 @@ class User(db.Model,UserMixin): #, EntityBase):
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(20), nullable=False)
     messages = db.relationship('Message',back_populates='author', cascade='all')
-    # head = db.Column(db.String(100))
-    # nickName = db.Column(db.String(100))
-    # status = db.Column(db.Date)
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
     def set_password(self, password):
