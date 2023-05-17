2b9621c028117a0a7de9e93d14439d0df42af35f
Mr.Wuliu_DESKTOP <mr_wuliu@foxmail.com>
2023-05-17 09:45:41 +0800

    版本更新

--- flaskr/blueprint/home.py
+++ flaskr/blueprint/home.py
@@ -16,4 +16,13 @@ def home():
         username = form.username.data
         # 重設username欄位
         form.username.data = ''
-    return render_template('common/B_base.html', form=form, username=username)
+    # return render_template('common/B_base.html', form=form, username=username)
+    news = ['1','2','3','4']
+    rank_list = ['榜一大哥','榜二大哥','榜三大哥']
+    hot_posts = ['热议','震惊','速速点击']
+    check_in_status = False
+    return render_template('common/base.html',
+                           news=news,
+                           ranklist=rank_list,
+                           hotposts=hot_posts,
+                           checkstatus=check_in_status)
