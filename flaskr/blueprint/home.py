8c250e4ca1698caeb61be7907fb01ff4ae41809e
Mr.Wuliu_DESKTOP <mr_wuliu@foxmail.com>
2023-05-22 08:57:13 +0800

    摸鱼派主页

--- flaskr/blueprint/home.py
+++ flaskr/blueprint/home.py
@@ -16,13 +16,19 @@ def home():
         username = form.username.data
         # 重設username欄位
         form.username.data = ''
-    # return render_template('common/B_base.html', form=form, username=username)
     news = ['1','2','3','4']
     rank_list = ['榜一大哥','榜二大哥','榜三大哥']
     hot_posts = ['热议','震惊','速速点击']
     check_in_status = False
-    return render_template('common/base.html',
+    home_card = True
+    chat_room = ['聊天室1','聊天室2','聊天室3','聊天室4']
+    label = ['1','2','3','4']
+    QA = ['如何学号前端','生活是什么','标签随机摆放']
+
+    note = ['生活小贴士','js的100个技巧','工具网站']
+    return render_template('chat/main.html',
                            news=news,
                            ranklist=rank_list,
                            hotposts=hot_posts,
+                           home_card=home_card,
                            checkstatus=check_in_status)
